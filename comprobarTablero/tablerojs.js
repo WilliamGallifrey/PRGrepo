dc4669bb762c4c72c8f366a7168e9b81adfcbe67
WilliamGallifrey <guigarro23@gmail.com>
2018-10-22 20:52:52 +0200

    comprobartablero

--- comprobarTablero/tablerojs.js
+++ comprobarTablero/tablerojs.js
@@ -1,17 +1,34 @@
 
+
+
 var fil = Number(prompt("Introduce el valor de fila"));
-
-while(fil < 1 || fil > 6)
-	{	alert("El valor de fila debe estar entre 1 y 6");
-		fil = Number(prompt("Introduce el valor de fila"));
-	}
-
 var col = Number(prompt("Introduce el valor de columna"));
-while(col < 1 || col > 6) 
-	{
-		alert("El valor de columna debe estar entre 1 y 6");
-		col = Number(prompt("Introduce el valor de columna"));
+var enfil = true;
+var encol = true;
 
-	}
 
-console.log("fila: " + fil + " columna: " + col);
+function compruebaTablero (fila, columna)
+{
+	if(fila < 1 || fila > 6) enfil = false;
+
+	if(columna < 1 || columna > 6) encol = false;
+
+
+	if(!encol)
+		{
+			console.log("la columna  no es válida");return false;
+		}
+
+	if(!enfil) 
+		{
+			console.log("la fila  no es válida");return false;
+		}
+		
+	return true;
+
+
+}
+
+
+
+console.log(compruebaTablero(fil,col));
